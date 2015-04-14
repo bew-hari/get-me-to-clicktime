48e9f0a7c00720be5c6efd2f4fc034c1648e1c82
Bew Harichanwong <bew.hari@gmail.com>
2015-04-13 22:01:00 -0500

    Clean up code

--- main.js
+++ main.js
@@ -10,11 +10,17 @@ var donutShop = undefined;
 
 var travelMode = undefined;
 var directionsDisplay;
-var directionsService; //= new google.maps.DirectionsService();
+var directionsService;
 
 function initialize() {
+  // stretch map and directions div to page height
+  $('#map-canvas').css('height', $(window).height());
+  $('#directions').css('height', $(window).height());
+
+  var travelMode = 'DRIVING';
+
   var mapOptions = {
-    // default location is NU Campus
+    // default location at NU Campus
     center: { lat: 42.055984, lng: -87.675171 },
     zoom: 18,
     zoomControl: true,
@@ -30,25 +36,18 @@ function initialize() {
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   
+  // initialize directions service
   directionsService = new google.maps.DirectionsService();
   directionsDisplay = new google.maps.DirectionsRenderer();
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById('directions'));
 
-  $('#map-canvas').css('height', $(window).height());
-  $('#directions').css('height', $(window).height());
 
   geocoder = new google.maps.Geocoder();
   geocoder.geocode( { 'address': destAddress}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       destLocation = results[0].geometry.location;
-      /*destMarker = new google.maps.Marker({
-        map: map,
-        position: destLocation,
-        title: 'Destination'
-      });*/
 
-      nearbyPlaces = {}
       findPlaces(destLocation, '1000', 'coffee');
       findPlaces(destLocation, '1000', 'donuts');
     } else {
@@ -58,20 +57,15 @@ function initialize() {
   getCurrentLocation();
 }
 
-
+// find user location with HTML5 geolocation 
 function getCurrentLocation() {
   $('#directions').html('Getting your location...');
   if(navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       myLocation = new google.maps.LatLng(position.coords.latitude,
                                           position.coords.longitude);
-      /*myMarker = new google.maps.Marker({
-        position: myLocation,
-        map: map,
-        title: 'Me'
-      });
-      map.setCenter(myLocation);*/
-      calcRoute();
+
+      getDirections();
 
     }, function() {
       handleNoGeolocation(true);
@@ -100,18 +94,62 @@ function handleNoGeolocation(errorFlag) {
   map.setCenter(options.position);
 }
 
+// find nearby bakery, cafes
+function findPlaces(location, radius, keyword) {
+  // find places based on supplied keyword and ranked by prominence
+  var request = {
+    location: location,
+    radius: radius,
+    keyword: keyword,
+    rankBy: google.maps.places.RankBy.PROMINENCE,
+    types: ['bakery', 'cafe', 'store'],
+    openNow: true
+  };
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, function (results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-function calcRoute() {
+      switch(keyword) {
+        case 'coffee':
+          coffeeShop = results[0]; // take first result
+          break;
+        case 'donut':
+        case 'donuts':
+          donutShop = results[0]; // take first result
+          break;
+        default:
+          coffeeShop = undefined;
+          donutShop = undefined;
+          $('#directions').html('Oops! Something went wrong.');
+      }
+
+      getDirections();
+    } else {
+      alert("Search returned status: " + status);
+    }
+  });
+}
+
+
+function getDirections() {
   // not all parameters are ready
   if (!myLocation || !coffeeShop || !donutShop) {
     return;
   }
-  //alert('here');
+
+  // update on progress
   $('#directions').html('Calculating route...');
 
-  calcTransitRoute();
-  return;
+  // call appropriate function based on travel mode
+  if (travelMode == 'TRANSIT') {
+    calcTransitRoute();
+  } else {
+    calcRoute();
+  }
+}
 
+function calcRoute() {
+  // construct waypoints for request
   var waypts = [];
   waypts.push({
     location: donutShop.geometry.location,
@@ -137,7 +175,7 @@ function calcRoute() {
   });
 }
 
-// calculate route manually for TRANSIT
+// calculate route manually for TRANSIT (sadly, no waypoints allowed)
 function calcTransitRoute() {
   var request = {
     origin: myLocation,
@@ -162,7 +200,7 @@ function calcTransitRoute() {
           };
           directionsService.route(request, function(toDest, status) {
             if (status == google.maps.DirectionsStatus.OK) {
-              // combine legs of whole trip
+              // combine segments of trip
               var legs = toDonutShop.routes[0].legs
                           .concat(toCoffeeShop.routes[0].legs)
                           .concat(toDest.routes[0].legs);
@@ -178,41 +216,6 @@ function calcTransitRoute() {
           });
         }
       });
-    }
-  });
-}
-
-
-// find nearby bakery, cafes
-function findPlaces(location, radius, keyword) {
-  var request = {
-    location: location,
-    radius: radius,
-    keyword: keyword,
-    rankBy: google.maps.places.RankBy.PROMINENCE,
-    types: ['bakery', 'cafe', 'store'],
-    openNow: true
-  };
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, function (results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-
-      switch(keyword) {
-        case 'coffee':
-          coffeeShop = results[0]; // take first result
-          break;
-        case 'donut':
-        case 'donuts':
-          donutShop = results[0]; // take first result
-          break;
-        default:
-          coffeeShop = undefined;
-          donutShop = undefined;
-      }
-
-      calcRoute();
-    } else {
-      alert("Search returned status: " + status);
     }
   });
 }
